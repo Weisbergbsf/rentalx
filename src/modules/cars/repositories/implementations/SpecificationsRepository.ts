35f65479c43c259d0b7c313cb73b169329d33d60
Weisberg Barros <weisbergbsf@gmail.com>
2022-08-07 15:44:47 -0300

    git commit -m "first commit"

---
+++ src/modules/cars/repositories/implementations/SpecificationsRepository.ts
@@ -0,0 +1,32 @@
+import { Specification } from "../../model/Specification";
+import {
+  ICreateSpecificationsDTO,
+  ISpecificationsRepository,
+} from "../ISpecificationsRepository";
+
+class SpecificationsRepository implements ISpecificationsRepository {
+  private specifications: Specification[];
+
+  constructor() {
+    this.specifications = [];
+  }
+
+  create({ name, description }: ICreateSpecificationsDTO): void {
+    const specification = new Specification();
+
+    Object.assign(specification, {
+      name,
+      description,
+      created_at: new Date(),
+    });
+    this.specifications.push(specification);
+  }
+
+  findByName(name: string): Specification {
+    return this.specifications.find(
+      (specification) => specification.name === name
+    );
+  }
+}
+
+export { SpecificationsRepository };
---
+++ src/routes/specifications.routes.ts
@@ -0,0 +1,10 @@
+import { Router } from "express";
+import { createSpecificationController } from "../modules/cars/useCases/createSpecification";
+
+const specificationsRoutes = Router();
+
+specificationsRoutes.post("/", (request, response) => {
+  createSpecificationController.handle(request, response);
+});
+
+export { specificationsRoutes };
